9819c10f10c6f92f0f75922cc2bf246a1152cf8f
rockwoodgaragedoors <jay@rockwoodgaragedoors.ca>
2025-07-02 08:41:44 -0400

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -262,6 +262,7 @@ async function fetchMondayOrderStatus() {
                             }
                         }
                     }
+                  }  
                 `
             })
         });
